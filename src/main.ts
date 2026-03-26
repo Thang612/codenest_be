e6508d7fa0734796777ad1de7899d8054abdcd36
Thang612 <thangdt61203@gmail.com>
2026-03-26 21:41:07 +0700

    deploy

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://codenest-fe-xdck.vercel.app',
+    ],
     credentials: true,
   });
 
